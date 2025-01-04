db42e4595068f11f433e880975b5eb3609e8db04
Sudle-BTK <sudle@sudle2008.top>
2025-01-04 23:31:44 +0800

    upload js

---
+++ iframe_create.js
@@ -0,0 +1,13 @@
+﻿function CreateIframe (src,h){
+  var iframe = document.createElement ('iframe');
+  iframe.src = src;
+  iframe.width ='100%';
+  if (h === undefined){
+    h = window.innerHeight || document.ducumentElement.clientHeight;
+  }
+  iframe.height = h;
+  iframe.frameBorder = '0';
+
+  document.body.appendChild (iframe)
+}
+
