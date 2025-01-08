882094488613f50bd6a158a232b9be27900214e7
Sudle-BTK <sudle@sudle2008.top>
2025-01-08 22:15:28 +0800

    update js -add fd -no need to type https://

--- iframe_create.js
+++ iframe_create.js
@@ -1,4 +1,4 @@
-﻿function CreateIframe (src,h){
+﻿function CreateIframe (src,fd,h){
   var iframe = document.createElement ('iframe');
   iframe.src = src;
   iframe.width ='100%';
@@ -7,7 +7,12 @@
   }
   iframe.height = h;
   iframe.frameBorder = '0';
-
+  if (fd){
+    iframe.sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts";
+  }
+  if (!src.includes("//")){
+    src = "https://" + src;
+  }
   document.body.appendChild(iframe);
   console.log("succeed to add Iframe");
   return 1;
